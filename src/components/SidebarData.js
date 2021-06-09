13655b384c3fd7953b4137457b29cf3a9432dca7
Nicolás Longo <longo.gnr@hotmail.com>
2021-06-08 21:03:40 -0300

    Trate de llevarme el styling a un CSS pero obviamente no me salio

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
 import * as RiIcons from 'react-icons/ri';
@@ -11,19 +10,6 @@ export const SidebarData = [
     icon: <AiIcons.AiFillHome />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
-
-    // subNav: [
-    //   {
-    //     title: 'Users',
-    //     path: '/overview/users',
-    //     icon: <IoIcons.IoIosPaper />
-    //   },
-    //   {
-    //     title: 'Revenue',
-    //     path: '/overview/revenue',
-    //     icon: <IoIcons.IoIosPaper />
-    //   }
-    // ]
   },
   {
     title: 'Denuncias',
