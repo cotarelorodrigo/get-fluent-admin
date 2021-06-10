86bffa4adc3544419741cd77160afa7ca100ae1e
Rodrigo Cotarelo <cotarelorodrigo@gmail.com>
2021-06-10 19:49:05 -0300

    init component lista denuncias

--- src/screens/Denuncias/Denuncias.js
+++ src/screens/Denuncias/Denuncias.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import DetallePorDenunciado from "../DetallePorDenunciado/DetallePorDenunciado"
+import ListaDenunciados from '../../components/ListaDenunciados'
 import "./Denuncias.css"
 
-export default function Denuncias({history}) {
+export default function Denuncias() {
 
-    // const denunciado = "longo.gnr@hotmail.com"
-    const denunciado = "rorroeche@hotmail.com"
 
     return (
-        <>
-            <DetallePorDenunciado history={history} denunciado={denunciado}/>
-        </>
+        <div>
+            <ListaDenunciados />
+        </div>
     )
 }
